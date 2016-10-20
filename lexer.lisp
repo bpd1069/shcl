11af358f65c5e928bea7c1ac2fcc23fccc76030e
Brad Jensen <brad@bradjensen.net>
2016-10-19 18:47:07 -0700

    Remove print-object methods

--- lexer.lisp
+++ lexer.lisp
@@ -14,8 +14,6 @@
           :initform nil
           :accessor token-value
           :initarg :value)))
-(defmethod print-object ((token token) stream)
-  (format stream "#<~A ~W>" (class-name (class-of token)) (token-value token)))
 (define-make-load-form-for-class token)
 
 (defclass a-word (token)
@@ -28,8 +26,6 @@
     :accessor simple-word-text
     :initform (error "required")
     :type string)))
-(defmethod print-object ((simple-word simple-word) stream)
-  (format stream "#<~A ~S>" (class-name (class-of simple-word)) (simple-word-text simple-word)))
 (define-make-load-form-for-class simple-word)
 
 (defclass assignment-word (a-word)
@@ -43,8 +39,6 @@
     :initarg :value-word
     :initform (error "required")
     :accessor assignment-word-value-word)))
-(defmethod print-object ((word assignment-word) stream)
-  (format stream "#<~A ~S = ~S>" (class-name (class-of word)) (assignment-word-name word) (assignment-word-value-word word)))
 (define-make-load-form-for-class assignment-word)
 
 (defclass name (simple-word)
